55c756d8d07dc29900608aedef71d9e93f82bdfe
anne <richardson.ae@gmail.com>
2017-11-04 10:16:49 -0500

    Adds CRUD methods to controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,10 +13,38 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      redirect_to edit_article_path(@article)
+    end
   end
 
   def show
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
   end
+
+  def edit
+    # @article = Article.find(params[:id])
+  end
+
+  def update
+    # @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      redirect_to edit_article_path(@article)
+    end
+  end
+
+  def destroy
+    # @article = Article.find(params[:id])
+    if @article.destroy
+      redirect_to articles_path
+    else
+      redirect_to edit_article_path(@article)
+    end
+  end
+
 end
