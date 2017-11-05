31b0ccc0a6c65e08e0f4ad8396e9f546a9ff5e82
anne <richardson.ae@gmail.com>
2017-11-05 11:32:23 -0600

    Adds index and delete ability to tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,21 @@
 class TagsController < ApplicationController
 
+  def index
+    @tags = Tag.all.order('name')
+  end
+
   def show
     @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    if @tag.destroy
+      redirect_to tags_path
+      flash.notice = "Tag '#{@tag.name}' has been Deleted!"
+    else
+      redirect_to tags_path
+      flash.notice = "Tag '#{@tag.name}' was NOT Deleted."
+    end
   end
 end
