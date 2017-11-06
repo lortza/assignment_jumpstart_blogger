d51e616e26e633031cb2300e5cf7343aa2bda70e
anne <richardson.ae@gmail.com>
2017-11-06 15:10:30 -0600

    Implements sessions for user log in/out

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -71,4 +71,4 @@ class AuthorsController < ApplicationController
     def author_params
       params.require(:author).permit(:username, :email, :password, :password_confirmation)
     end
-end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'articles#index'
+
+  resources :author_sessions, only: [ :new, :create, :destroy ]
+  get 'login'  => 'author_sessions#new'
+  get 'logout' => 'author_sessions#destroy'
+
   resources :authors
   resources :articles do
     resources :comments
